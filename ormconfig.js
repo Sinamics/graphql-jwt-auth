76cb42a48a779f055544e1cbc249d88c76a677eb
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2021-03-22 20:29:39 +0100

    added useUnifiedTopology

--- ormconfig.js
+++ ormconfig.js
@@ -5,6 +5,7 @@ module.exports = [
     database: 'graphjwtauth',
     synchronize: true,
     logging: true,
+    useUnifiedTopology: true,
     entities: ['backend/graphql/entity/**/*.ts'],
     migrations: ['backend/graphql/migration/**/*.js'],
     subscribers: ['backend/graphql/subscriber/**/*.ts'],
@@ -20,6 +21,7 @@ module.exports = [
     database: 'graphjwtauth',
     synchronize: true, // switch this to false once you have the initial tables created and use migrations instead
     logging: false,
+    useUnifiedTopology: true,
     entities: ['dist/graphql/entity/*.js'],
     migrations: ['dist/graphql/migration/*.js'],
     subscribers: ['dist/graphql/subscriber/*.js'],
