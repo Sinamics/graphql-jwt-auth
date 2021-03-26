c41c45f3989e7bcc3cd0d2c79c56be824414bb5c
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2021-03-26 16:13:34 +0100

    semantic dev

--- frontend/views/landingPage.tsx
+++ frontend/views/landingPage.tsx
@@ -1,36 +1,27 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Button, Divider, Grid, Header } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const LandingPage = (): JSX.Element => {
   return (
-    <div>Hey</div>
-    // <Grid padded centered>
-    //   <Grid.Row>
-    //     <Grid.Column width='4'>
-    //       <Divider clearing />
-    //       <Header color='teal' as='h1' content='GraphQl-jwt-auth Boilerplate' textAlign='center' subheader='enjoy!' />
-    //     </Grid.Column>
-    //   </Grid.Row>
-
-    //   <Grid.Row>
-    //     <Grid.Column width='2'>
-    //       <Link to='login'>
-    //         <Button color='teal'>Login</Button>
-    //       </Link>
-    //     </Grid.Column>
-    //     <Grid.Column width='2' textAlign='right'>
-    //       <Link to='register'>
-    //         <Button color='teal'>Register</Button>
-    //       </Link>
-    //     </Grid.Column>
-    //   </Grid.Row>
-    //   <Grid.Row>
-    //     <Grid.Column width='4' textAlign='right'>
-    //       <Divider />
-    //     </Grid.Column>
-    //   </Grid.Row>
-    // </Grid>
+    <div className='py-20 h-screen bg-gray-100 px-4 sm:px-6 lg:px-8'>
+      <div className='row-auto text-center w-full'>
+        <p className='text-5xl py-5'>GraphQl-jwt-auth Boilerplate</p>
+      </div>
+      <div className='container mx-auto'>
+        <div className='flex justify-center'>
+          <div className='pr-10'>
+            <Link to='login'>
+              <button className='p-3 bg-green-400 hover:bg-green-600 rounded-lg text-lg focus:outline-none shadow-lg'>Login</button>
+            </Link>
+          </div>
+          <div className=''>
+            <Link to='register'>
+              <button className='p-3 bg-green-400 hover:bg-green-600 rounded-lg text-lg focus:outline-none shadow-lg'>Register</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
